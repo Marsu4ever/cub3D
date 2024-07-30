f4526a6e23ae81d638c5ab8f5f8872f8667e3ba5
Simos Tigkas <stigkas@c3r3p1.hive.fi>
2024-07-30 11:04:05 +0300

    ADD: libs for the external functions, basic start at main.c

--- includes/cube3d.h
+++ includes/cube3d.h
@@ -1,18 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   cube3d.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: stigkas <stigkas@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/30 10:57:59 by stigkas           #+#    #+#             */
-/*   Updated: 2024/07/30 11:02:02 by stigkas          ###   ########.fr       */
+/*   Created: 2024/07/30 10:48:25 by stigkas           #+#    #+#             */
+/*   Updated: 2024/07/30 11:01:42 by stigkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/cube3d.h"
+#ifndef CUBE3D_H
+# define CUBE3D_H
 
-int main (int ac, char **av)
-{
-    
-}
+# include <math.h> //for all the math functions we will need in raycasting
+# include <fcntl.h> //open
+# include <unistd.h> //write, read, close
+# include <stdio.h> //perror, printf
+# include <stdlib.h> //exit, free, malloc
+# include <string.h> //strerror
+
+
+
+
+
+#endif
