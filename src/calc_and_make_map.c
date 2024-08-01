d07db9a04c9920d9cfd5193b52b61b8097873366
Markus Korpela <mkorpela@c2r5p11.hive.fi>
2024-08-01 10:35:09 +0300

    ADD: mlx_key_hook & refactored new .c files [make_map.c $ mlx_functions.c]

---
+++ src/calc_and_make_map.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   calc_and_make_map.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkorpela <mkorpela@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/01 10:23:05 by mkorpela          #+#    #+#             */
+/*   Updated: 2024/08/01 10:23:38 by mkorpela         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cube3d.h"
+
+static void make_map(t_game *game, int num_of_rows_in_map, char **argv)
+{
+	int		fd;
+	char	**map;
+	int		i;
+
+	map = malloc(sizeof(char *) * (num_of_rows_in_map + 1));
+	if (map == NULL)
+		exit (1);
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		ft_putstr_fd("Error\nFile didn't open. ", 2);
+		ft_putstr_fd("Was it perchance a misspelt file you added there?\n", 2);
+		exit (1);
+	}
+	i = 0;
+	while (i < num_of_rows_in_map)
+	{
+		map[i] = get_next_line(fd);
+		if (map[i] == NULL)
+			// free_incomplete_map_and_exit(map, i);
+		i++;
+	}
+	map[i] = NULL;
+	close (fd);
+	game->map = map;
+}
+
+static int	number_of_rows_in_map(char **argv)
+{
+	int		fd;
+	int		num_of_rows_in_map;
+	char	*string;
+
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		ft_putstr_fd("Error\nFile didn't open. ", 2);
+		ft_putstr_fd("Was it perchance a misspelt file you added there?\n", 2);
+		exit (1);
+	}
+	num_of_rows_in_map = 0;
+	string = "let's get into the while loop";
+	while (string)
+	{
+		string = get_next_line(fd);
+		// printf("%s", string);
+		if (string)
+			num_of_rows_in_map++;
+		free (string);
+	}
+	close (fd);
+	return (num_of_rows_in_map);
+}
+
+
+void calc_and_make_map(t_game *game, char **av)
+{
+	int	num_of_rows;
+	
+	num_of_rows = number_of_rows_in_map(av);
+	make_map(game, num_of_rows, av);
+}
---
+++ src/mlx_functions.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mlx_functions.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkorpela <mkorpela@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/01 10:02:05 by mkorpela          #+#    #+#             */
+/*   Updated: 2024/08/01 10:28:50 by mkorpela         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cube3d.h"
+
+void	move_hook(mlx_key_data_t keydata, void *game_from_key_hook)
+{
+	t_game	*game;
+
+	game = (t_game *) game_from_key_hook;
+	if (mlx_is_key_down(game->mlx, MLX_KEY_ESCAPE))
+		mlx_close_window(game->mlx);
+	if (keydata.key == MLX_KEY_RIGHT && keydata.action == MLX_PRESS)
+		printf("Key Press: Right arrow\n");                         //Ray tracing 4 simos
+	if (keydata.key == MLX_KEY_LEFT && keydata.action == MLX_PRESS)
+		printf("Key Press: Left arrow\n");							//Ray tracing
+	if (keydata.key == MLX_KEY_W && keydata.action == MLX_PRESS)
+		printf("Key Press: W arrow\n");								//Ray tracing
+	if (keydata.key == MLX_KEY_S && keydata.action == MLX_PRESS)
+		printf("Key Press: S arrow\n");								//Ray tracing
+	if (keydata.key == MLX_KEY_D && keydata.action == MLX_PRESS)
+		printf("Key Press: D arrow\n");								//Ray tracing
+	if (keydata.key == MLX_KEY_A && keydata.action == MLX_PRESS)
+		printf("Key Press: A arrow\n");								//Ray tracing
+}
+
+
+void    mlx_functions(t_game *game)
+{
+    game->mlx = mlx_init(960, 540, "cub3D", false); /*game_width = 1300 and  game_height = 900 - (fshields)*/
+	game->image = mlx_new_image(game->mlx, 960, 540);
+	mlx_load_png("./textures/NO.png");
+	mlx_load_png("./textures/EA.png");
+	mlx_load_png("./textures/SO.png");
+	mlx_load_png("./textures/WE.png");
+	mlx_image_to_window(game->mlx, game->image, 0, 0);
+	mlx_key_hook(game->mlx, move_hook, game);
+	mlx_loop(game->mlx);
+	mlx_terminate(game->mlx);
+}
