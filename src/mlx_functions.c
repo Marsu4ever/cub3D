b2133984f4f760065c3e944b4dbdd2b086392d76
Simos Tigkas <stigkas@c2r5p7.hive.fi>
2024-08-09 12:44:31 +0300

    stupid comment fix

--- src/mlx_functions.c
+++ src/mlx_functions.c
@@ -6,7 +6,7 @@
 /*   By: stigkas <stigkas@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/01 10:02:05 by mkorpela          #+#    #+#             */
-/*   Updated: 2024/08/09 12:09:41 by stigkas          ###   ########.fr       */
+/*   Updated: 2024/08/09 12:25:37 by stigkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,30 +20,29 @@ void	move_hook(mlx_key_data_t keydata, void *game_from_key_hook)
 	if (mlx_is_key_down(game->mlx, MLX_KEY_ESCAPE))
 		mlx_close_window(game->mlx);
 	if (keydata.key == MLX_KEY_RIGHT && keydata.action == MLX_PRESS)
-		printf("Key Press: -------->\n");                         //Ray tracing 4 simos
+		printf("Key Press: -------->\n"); //Raycasting 4 simos
 	if (keydata.key == MLX_KEY_LEFT && keydata.action == MLX_PRESS)
-		printf("Key Press: <--------\n");							//Ray tracing
+		printf("Key Press: <--------\n"); //Raycasting
 	if (keydata.key == MLX_KEY_W && keydata.action == MLX_PRESS)
-		printf("Key Press: W\n");								//Ray tracing
+		printf("Key Press: W\n"); //Raycasting
 	if (keydata.key == MLX_KEY_S && keydata.action == MLX_PRESS)
-		printf("Key Press: S\n");								//Ray tracing
+		printf("Key Press: S\n"); //Raycasting
 	if (keydata.key == MLX_KEY_D && keydata.action == MLX_PRESS)
-		printf("Key Press: D\n");								//Ray tracing
+		printf("Key Press: D\n"); //Raycasting
 	if (keydata.key == MLX_KEY_A && keydata.action == MLX_PRESS)
-		printf("Key Press: A\n");								//Ray tracing
-
+		printf("Key Press: A\n"); //Raycasting
 	if (keydata.key == MLX_KEY_RIGHT && keydata.action == MLX_REPEAT)
-		printf("Key Press: -------->\n");                         //Ray tracing 4 simos
+		printf("Key Press: -------->\n"); //Raycasting
 	if (keydata.key == MLX_KEY_LEFT && keydata.action == MLX_REPEAT)
-		printf("Key Press: <--------\n");							//Ray tracing
+		printf("Key Press: <--------\n"); //Raycasting
 	if (keydata.key == MLX_KEY_W && keydata.action == MLX_REPEAT)
-		printf("Key Press: W\n");								//Ray tracing
+		printf("Key Press: W\n"); //Raycasting
 	if (keydata.key == MLX_KEY_S && keydata.action == MLX_REPEAT)
-		printf("Key Press: S\n");								//Ray tracing
+		printf("Key Press: S\n"); //Raycasting
 	if (keydata.key == MLX_KEY_D && keydata.action == MLX_REPEAT)
-		printf("Key Press: D\n");								//Ray tracing
+		printf("Key Press: D\n"); //Raycasting
 	if (keydata.key == MLX_KEY_A && keydata.action == MLX_REPEAT)
-		printf("Key Press: A\n");
+		printf("Key Press: A\n"); //Raycasting
 }
 
 
