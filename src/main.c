021579bbe9927bbcc44187bdd8154c330c7293e9
Simos Tigkas <stigkas@c1r4p1.hive.fi>
2024-09-18 14:16:25 +0300

    norminetted but Markus check some comments that are lef ig tehy can be deleted

--- src/main.c
+++ src/main.c
@@ -6,32 +6,32 @@
 /*   By: stigkas <stigkas@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/30 10:57:59 by stigkas           #+#    #+#             */
-/*   Updated: 2024/09/18 13:19:40 by stigkas          ###   ########.fr       */
+/*   Updated: 2024/09/18 14:10:30 by stigkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cube3d.h"
 
-int pos_valid(t_vars *game)
+int	pos_valid(t_vars *game)
 {
-    int i;
-    int j;
-    int x;
-    int y;
+	int	i;
+	int	j;
+	int	x;
+	int	y;
 
-    i = 0;
-    j = 0;
-    x = (int)game->pl->x_pos;
-    y = (int)game->pl->y_pos;
-    while (game->map[j])
-        j++;
-    if (j < y)
-        return (0);
-    while (game->map[y][i])
-        i++;
-    if (i < x)
-        return (0);
-    return (1);
+	i = 0;
+	j = 0;
+	x = (int)game->pl->x_pos;
+	y = (int)game->pl->y_pos;
+	while (game->map[j])
+		j++;
+	if (j < y)
+		return (0);
+	while (game->map[y][i])
+		i++;
+	if (i < x)
+		return (0);
+	return (1);
 }
 
 void	close_program(t_vars *game)
